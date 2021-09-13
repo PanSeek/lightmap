c70e375fc817341049553b7fc2d898e747ef4a83
PanSeek <35432222+PanSeek@users.noreply.github.com>
2021-09-13 21:16:31 +0300

    Add files via upload

---
+++ dllmain.cpp
@@ -0,0 +1,16 @@
+﻿#include <windows.h>
+
+class CMain {
+public:
+	CMain() {
+		VirtualProtect(reinterpret_cast<LPVOID>(0x005429F9), 2, PAGE_READWRITE, &oldProt);
+		memset(reinterpret_cast<void*>(0x73558B), 0x90, 0x2);
+		VirtualProtect(reinterpret_cast<LPVOID>(0x005429F9), 2, oldProt, &oldProt);
+	};
+	~CMain() {
+		VirtualProtect(reinterpret_cast<LPVOID>(0x005429F9), 2, PAGE_READWRITE, &oldProt);
+		memcpy(reinterpret_cast<void*>(0x73558B), "\x74\x3C", 0x2);
+		VirtualProtect(reinterpret_cast<LPVOID>(0x005429F9), 2, oldProt, &oldProt);
+	};
+	DWORD oldProt;
+}; CMain plugin;
