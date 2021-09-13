8b36ab9c205102888b793c46430dd2b82633d80c
PanSeek <35432222+PanSeek@users.noreply.github.com>
2021-09-13 23:43:35 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -3,14 +3,14 @@
 class CMain {
 public:
 	CMain() {
-		VirtualProtect(reinterpret_cast<LPVOID>(0x005429F9), 2, PAGE_READWRITE, &oldProt);
+		VirtualProtect(reinterpret_cast<LPVOID>(0x73558B), 2, PAGE_READWRITE, &oldProt);
 		memset(reinterpret_cast<void*>(0x73558B), 0x90, 0x2);
-		VirtualProtect(reinterpret_cast<LPVOID>(0x005429F9), 2, oldProt, &oldProt);
+		VirtualProtect(reinterpret_cast<LPVOID>(0x73558B), 2, oldProt, &oldProt);
 	};
 	~CMain() {
-		VirtualProtect(reinterpret_cast<LPVOID>(0x005429F9), 2, PAGE_READWRITE, &oldProt);
+		VirtualProtect(reinterpret_cast<LPVOID>(0x73558B), 2, PAGE_READWRITE, &oldProt);
 		memcpy(reinterpret_cast<void*>(0x73558B), "\x74\x3C", 0x2);
-		VirtualProtect(reinterpret_cast<LPVOID>(0x005429F9), 2, oldProt, &oldProt);
+		VirtualProtect(reinterpret_cast<LPVOID>(0x73558B), 2, oldProt, &oldProt);
 	};
 	DWORD oldProt;
 }; CMain plugin;
